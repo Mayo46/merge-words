fb74387ba4258b9478f2cd15546bfbe787acbb3c
--replace-all <mubasharmayo46@gmail.com>
2021-09-07 19:40:43 +0500

    feedback

--- pages/index.vue
+++ pages/index.vue
@@ -339,7 +339,7 @@ export default {
     Domain(){
       this.firstText= " iphone\n" + "ipad\n" + "ipod\n" + "imac\n" + "macbook"
       this.secondText="world\n" + "life\n" + "web\n" + "planet\n" + "hub\n" + "center\n" + "club\n" + "central\n" + "spot\n" + "base\n" + "stuff"
-      this.thirdText=".com\n" + ".net\n" + ".org";
+      this.thirdText=".com\n" + ".net\n" + ".org"
       setTimeout(() => this.mergeText(), 1000)
     
     },
@@ -347,11 +347,13 @@ export default {
       this.firstText="mountaineering\n" + "climbing\n" + "hiking\n" + "trekking"
       this.secondText="websites\n" + "links\n" + "\"add url\"\n" + "\"suggest a site\""
       this.thirdText="intitle:list\n" + "inurl:resources\n" + "OR \"suggest URL\"\n" + "OR resources"
+       setTimeout(() => this.mergeText(), 1000)
     },
     adwords(){
       this.firstText="ladies\n" + "women\n" + "designer\n" + "fashion"
       this.secondText="shoes\n" + "boots\n" + "sandals\n" + "stiletto heels"
       this.thirdText="New York\n" + "New Jersey\n" + "Long Island City\n" + "Manhattan"
+       setTimeout(() => this.mergeText(), 1000)
     },
     add(){
       let options = [] , field = '' , newArray = []
